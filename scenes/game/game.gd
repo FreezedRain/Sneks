9eb0cd2232619f392858073f46d0e479e7b1fdbd
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2022-05-15 17:21:32 +0300

    Loading levels

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,4 +1,20 @@
 ## Master class for everything related to gameplay
 extends Node2D
 
-# Spawn levels, handle win/lose states and transitions
+export (Array, Resource) var levels
+export (int) var level_idx
+
+const LEVEL_SCENE = preload("res://scenes/level/level.tscn")
+
+var current_level: Level
+
+func _ready():
+	load_level(level_idx)
+
+func load_level(idx: int):
+	if current_level:
+		current_level.queue_free()
+	var level_data = null #levels[idx]
+	current_level = LEVEL_SCENE.instance()
+	current_level.load_level(level_data)
+	add_child(current_level)
--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -1,7 +1,9 @@
 ## Everything related to a single level
-extends Node2D
+class_name Level extends Node2D
 
 onready var tiles = $Tiles
 onready var snakes = $Snakes.get_children()
 
 # Handle player turns and environment actions
+func load_level(data):
+    pass
