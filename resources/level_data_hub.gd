21a4701e0a244385e506015a50a443ea0b019874
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2022-05-22 20:21:36 +0300

    update hub parsing logic

--- resources/level_data_hub.gd
+++ resources/level_data_hub.gd
@@ -11,35 +11,37 @@ func parse_level():
 	for x in range(size.x):
 		var col = []
 		for y in range(size.y):
-			if lines[y + 1][x] == 'b':
-				if biome + 1 < TOTAL_BIOMES:
-					lines[y + 1][x] = 'b%d' % (biome + 1) 
-					col.append('b%d' % (biome + 1) )
-				else:
-					col.append('_')
-			else:
-				col.append(lines[y + 1][x])
-			#col.append('O')
+			col.append(lines[y + 1][x].split(' '))
 		level.append(col)
+			# if lines[y + 1][x] == 'b':
+			# 	if biome + 1 < TOTAL_BIOMES:
+			# 		lines[y + 1][x] = 'b%d' % (biome + 1) 
+			# 		col.append('b%d' % (biome + 1) )
+			# 	else:
+			# 		col.append('_')
+			# else:
+			# 	col.append(lines[y + 1][x])
+			#col.append('O')
+		
 
-	var count = 0
-	for x in range(1, size.x):
-		count = place_entrance(level, x, 0, count)
+# 	var count = 0
+# 	for x in range(1, size.x):
+# 		count = place_entrance(level, x, 0, count)
 
-	for y in range(1, size.y):
-		count = place_entrance(level, size.x - 1, y, count)
+# 	for y in range(1, size.y):
+# 		count = place_entrance(level, size.x - 1, y, count)
 
-	for x in range(1, size.x):
-		count = place_entrance(level, size.x - 1 - x, size.y - 1, count)
+# 	for x in range(1, size.x):
+# 		count = place_entrance(level, size.x - 1 - x, size.y - 1, count)
 
-	for y in range(1, size.y):
-		count = place_entrance(level, 0, size.y - 1 - y, count)
+# 	for y in range(1, size.y):
+# 		count = place_entrance(level, 0, size.y - 1 - y, count)
 
-func place_entrance(level, x, y, count):
-	if level[x][y] == 'e':
-		level[x][y] = 'e%s' % count
-		count += 1
-	return count
+# func place_entrance(level, x, y, count):
+# 	if level[x][y] == 'e':
+# 		level[x][y] = 'e%s' % count
+# 		count += 1
+# 	return count
 		# for y in range(size.y):
 		# 	if x == 0 or x == size.x - 1 or y == 0 or y == size.y - 1:
 		# 		# level[x][y] = '#'
