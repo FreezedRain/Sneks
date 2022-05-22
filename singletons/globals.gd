e0acbaf4bf65a1535e112cd11cfc516e3b086140
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2022-05-22 21:10:48 +0300

    added graveyard biome

--- singletons/globals.gd
+++ singletons/globals.gd
@@ -12,5 +12,6 @@ const COLOR_LETTERS = {
 const BIOME_RESOURCES = {
 	LevelData.Biome.DUSTY: preload("res://resources/biomes/biome_dusty.tres"),
 	LevelData.Biome.GRASSY: preload("res://resources/biomes/biome_grassy.tres"),
-	LevelData.Biome.ROCKY: preload("res://resources/biomes/biome_rocky.tres")
+	LevelData.Biome.ROCKY: preload("res://resources/biomes/biome_rocky.tres"),
+	LevelData.Biome.GRAVEYARD: preload("res://resources/biomes/biome_graveyard.tres")
 }
