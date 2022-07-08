535dd8fae68f0a24941000a27b13b245e6853f46
RainCatalyst <43936298+RainCatalyst@users.noreply.github.com>
2022-07-08 19:31:23 +0300

    Improved scaling

--- scripts/coolmathgames/CMGIntegration.gd
+++ scripts/coolmathgames/CMGIntegration.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const IS_CMG_BUILD = true
+var IS_DEBUG = OS.is_debug_build()
 
 var valid_domains = [
 	"www.coolmathgames.com",
@@ -16,31 +17,41 @@ var valid_domains = [
 	"m.coolmath-games.com"
 ]
 
+# func _ready():
+# 	JavaScript.eval("$(window).bind(\n'touchmove',\nfunction(e) {\ne.preventDefault();\n}\n);")
+
 func is_valid():
 	if !IS_CMG_BUILD:
 		return true
-	if OS.is_debug_build():
+	if IS_DEBUG:
 		return true
 	if not OS.get_name()=="HTML5":
 		return true
 	return valid_domains.has(get_domain())
 	
 func get_domain():
-	if !IS_CMG_BUILD: return ""
+	if !IS_CMG_BUILD:
+		return ""
 	return str(JavaScript.eval("document.location.host"))
 
 func game_start():
-	if !IS_CMG_BUILD: return
-	print("GAME START")
+	if !IS_CMG_BUILD:
+		return
+	if IS_DEBUG:
+		print("GAME START")
 	JavaScript.eval('parent.cmgGameEvent("start");', true)
 	
 func level_start(level_num):
-	if !IS_CMG_BUILD: return
-	print("LEVEL START: " + level_num)
+	if !IS_CMG_BUILD:
+		return
+	if IS_DEBUG:
+		print("LEVEL START: " + level_num)
 	JavaScript.eval('parent.cmgGameEvent("start","' + str(level_num) + '");', true)
 
 func level_restart(level_num):
-	if !IS_CMG_BUILD: return
-	print("LEVEL RESTART: " + level_num)
+	if !IS_CMG_BUILD:
+		return
+	if IS_DEBUG:
+		print("LEVEL RESTART: " + level_num)
 	JavaScript.eval('parent.cmgGameEvent("replay","' + str(level_num) + '");', true)
 
